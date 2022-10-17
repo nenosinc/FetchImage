84bfacf0a83ea8179e19b5db90483a1f27470fda
Sam Spencer <samspencer@hey.com>
2022-10-17 19:44:18 -0400

    Updated Firebase & Nuke Dependencies

--- Package.swift
+++ Package.swift
@@ -1,31 +1,26 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "FetchImage",
     platforms: [
-        .iOS("15.0")
+        .iOS(.v15)
     ],
     products: [
         .library(name: "FetchImage", targets: ["FetchImage"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/kean/Nuke.git",
-            .upToNextMajor(from: "10.9.0")
-        ),
-        .package(
-            url: "https://github.com/firebase/firebase-ios-sdk.git",
-            .upToNextMajor(from: "9.0.0")
-        )
+        .package(url: "https://github.com/kean/Nuke.git", from: "11.3.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "9.6.0")
     ],
     targets: [
-        .target(name: "FetchImage",
-                dependencies: [
-                    "Nuke",
-                    .product(name: "FirebaseStorage", package: "firebase-ios-sdk")
-                ],
-                path: "Source")
+        .target(
+            name: "FetchImage",
+            dependencies: [
+                "Nuke",
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk")
+            ],
+            path: "Source")
     ]
 )
